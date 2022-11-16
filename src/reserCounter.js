bd37f7a585beb29bf3fd4676b88a45347438b320
shlomi <shlomiasi1@gmail.com>
2022-11-16 13:33:36 +0200

    ResetButton

--- src/reserCounter.js
+++ src/reserCounter.js
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
 import "./styles.css";
 
-export default function App() {
+export default function resetCounter() {
   const [time, setTime] = useState(0);
   const reset = () => {
     setTime(0);
   };
   return (
     <div className="App">
-      <h1 className="mainHeading">Counter App With Reset</h1>
+      <h1 className="mainHeading">Blackout's Counter App With Reset</h1>
       <div className="count">{time}</div>
       <div className="buttons">
         <button
@@ -16,10 +16,16 @@ export default function App() {
             setTime(time + 1);
           }}
         >
-          Add 1 to the Counter
+          Add Blackout Time +
         </button>
-        <button onClick={reset}>Reset The Counter</button>
-
+        <button onClick={reset}>Reset Blackout Time</button>
+        <button
+          onClick={() => {
+            setTime(time - 1);
+          }}
+        >
+          Sub Blackout Time -
+        </button>
       </div>
     </div>
   );
